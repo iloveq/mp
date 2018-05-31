9f312623545af4ba62835cd815cabfdd098f13f1
woaigmz <woaigmz@gmail.com>
2018-05-31 13:14:06 +0800

    login fuc

--- router/user.js
+++ router/user.js
@@ -7,44 +7,73 @@ const JsonUtil = require('../utils/JsonUtil');
 
 
 
-//登陆
+//登陆 
 router.post("/login", (req, res) => {
 
-  UserModel.findOne();
-
-})
-
-//注册
-router.post("/register", (req, res) => {
-
-  UserModel.where({name:req.body.name}).findOne((err,success)=>{
-    if(err){
-      JsonUtil.response(res,'200',err,"返回错误");
-    }else{
-        if(StringUtil.isEmpty(success)){
-          if (StringUtil.isEmpty(req.body.name)||StringUtil.isEmpty(req.body.password)) {
-            JsonUtil.response(res,'200',{
-                  "username": req.body.name,
-                  "password": req.body.password
-                },"用户名密码不为空");
-          }else{
-            UserModel.create(req.body, (err, success) => {
-              if (err) {
-                JsonUtil.response(res,'200',err,"返回错误");
-              } else {
-                JsonUtil.response(res,'200',success,"返回成功");
-              }
-            });
-          }
-        }else{
-          JsonUtil.response(res,'201',success,"返回成功,账号已存在");
+  UserModel.where({ name: req.body.name }).findOne((err, success) => {
+    if (err) {
+      JsonUtil.response(res, '200', err, "返回错误");
+    } else {
+      if (!StringUtil.isEmpty(success)) {
+        if (req.body.password == success.password) {
+          JsonUtil.response(res, '200', {
+            "username": success.name,
+            "password": success.password,
+            "token":success.token
+          }, "登陆成功");
+        } else {
+          JsonUtil.response(res, '200', {
+            "username": req.body.name,
+            "password": req.body.password,
+            "token":""
+          }, "密码错误");
         }
+      }else{
+        JsonUtil.response(res, '200', {
+          "username": req.body.name,
+          "password": req.body.password,
+          "token":""
+        }, "用户不存在");
+      }
     }
 
   });
 
- 
-
 })
+
+
+
+
+//注册  code == '201' 已注册 
+router.post("/register", (req, res) => {
+  //name唯一     name 已注册？“已注册”：“creat user”;
+  UserModel.where({ name: req.body.name }).findOne((err, success) => {
+    if (err) {
+      JsonUtil.response(res, '200', err, "返回错误");
+    } else {
+      if (StringUtil.isEmpty(success)) {
+        console.log(success);
+        if (StringUtil.isEmpty(req.body.name) || StringUtil.isEmpty(req.body.password)) {
+          JsonUtil.response(res, '200', {
+            "name": req.body.name,
+            "password": req.body.password
+          }, "用户名密码不为空");
+        } else {
+          UserModel.create(req.body, (err, success) => {
+            if (err) {
+              JsonUtil.response(res, '200', err, "返回错误");
+            } else {
+              JsonUtil.response(res, '200', success, "返回成功");
+            }
+          });
+        }
+      } else {
+        JsonUtil.response(res, '201', success, "返回成功,账号已存在");
+      }
+    }
+  });
+})
+
+
 
 module.exports = router;
