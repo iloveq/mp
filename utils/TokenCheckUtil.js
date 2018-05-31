703e89b55aa50f03e845ad785012304378c40c83
woaigmz <woaigmz@gmail.com>
2018-05-31 17:17:14 +0800

    jwt: json web token create and check

--- utils/TokenCheckUtil.js
+++ utils/TokenCheckUtil.js
@@ -5,3 +5,26 @@ exports.getToken = function (obj,app) {
         expiresIn: 60 * 60 * 72
     });
 }
+
+//api 请求校验 token
+exports.verifyToken = function(req, res, next){
+
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if(token){
+        jwt.verify(token, req.app.get('jwt-secret') , function(err,decoded) {
+            if(err) {
+              return res.json({code:"401",message:'token错误'})
+            }else {
+              //解析成功
+              next()
+            }
+          })
+    }else{
+        return res.status(403).json({
+            code: '403',
+            message: '没有提供token！'
+        });
+    }
+
+
+}
