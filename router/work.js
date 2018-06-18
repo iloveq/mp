a8a75f790093b452c9d5be171e3a9dddcf03e047
woaigmz <woaigmz@gmail.com>
2018-06-18 15:57:41 +0800

    zongzi jie

--- router/work.js
+++ router/work.js
@@ -17,12 +17,14 @@ router.post('/upload', upload, function (req, res) {
         content: req.body.content,
         imgurl: 'http://' + req.headers.host + '/images/' + req.file.filename,
         userId: req.body.userId,
-        like: "",
-        share: ""
+        like: "0",
+        share: "0"
     }, (err, success) => {
         if (err) {
+            console.log(err);
             JsonUtil.response(res, '201', err, "返回错误");
         } else {
+            console.log(success);
             JsonUtil.response(res, '200', success, "上传图片成功");
         }
     });
