0dcf1f23b2ffa87c8cd2c9272a7eacb7286bdad6
woaigmz <woaigmz@gmail.com>
2018-06-21 14:02:43 +0800

    format

--- config/params.js
+++ config/params.js
@@ -1,6 +1,5 @@
-module.exports =  {
-    serverPort:3001,
-    mongodb : "mongodb://localhost:27017/mp",
-    jwtsecret:"5525278",
+module.exports = {
+    serverPort: 3001,
+    mongodb: "mongodb://localhost:27017/mp",
+    jwtsecret: "5525278",
 }
-  
